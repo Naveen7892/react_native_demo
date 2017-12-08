74f9464dd31726936b7f7891bc3f3f53a4f42c1c
Naveen Kumar <metanaveen@outlook.com>
2017-12-08 21:53:14 +0530

    Demo app - made it working

---
+++ index.js
@@ -0,0 +1,19 @@
+import { AppRegistry } from 'react-native';
+// import App from './App';
+// import App2 from './App2';
+// import AppTouchables from './AppTouchables';
+// import AppScrollView from './AppScrollView';
+// import AppListView from './AppListView';
+// import AppListView2 from './AppListView2';
+// import AppNetworking from './AppNetworking';
+
+// AppRegistry.registerComponent('helloworld', () => App);
+// AppRegistry.registerComponent('helloworld', () => App2);
+// AppRegistry.registerComponent('helloworld', () => AppTouchables);
+// AppRegistry.registerComponent('helloworld', () => AppScrollView);
+// AppRegistry.registerComponent('helloworld', () => AppListView);
+// AppRegistry.registerComponent('helloworld', () => AppListView2);
+// AppRegistry.registerComponent('helloworld', () => AppNetworking);
+
+import App from './App';
+AppRegistry.registerComponent('react_nav_demo', () => App);
